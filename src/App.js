7e5ca6840f740fd01a38ab25849d8b9df948d6f9
shafiuljony <shafiuljony12@gmail.com>
2022-06-16 21:34:31 +0600

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import {TooltipComponent} from "@syncfusion/ej2-react-popups";
+import {FiSettings} from "react-icons/fi";
 
 const App = () => {
+    const activeMenu = true;
   return(
-      <h1 className="underline text-3xl">App</h1>
+      <div>
+        <BrowserRouter>
+          <div className="flex relative dark:bg-main-dark-bg">
+            <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
+                <TooltipComponent content="Settings" position="Top">
+                    <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray" style={{ background: 'blue', borderRadius:'50%'}}>
+                        <FiSettings />
+                    </button>
+                </TooltipComponent>
+            </div>
+              {activeMenu ? (
+                  <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div>
+              ): (
+                  <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+              )}
+              <div className={
+                  `'dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72'
+                      : 'flex-2'}`
+              }>
+                  <div></div>
+              </div>
+          </div>
+        </BrowserRouter>
+      </div>
   )
 }
 
