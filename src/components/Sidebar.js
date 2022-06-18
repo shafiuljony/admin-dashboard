79bf5df4693633d5023461ef8bb8cea2a839d1c3
shafiuljony <shafiuljony12@gmail.com>
2022-06-18 20:59:52 +0600

    sidebar shoppy done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware} from "react-icons/si";
+import { MdOutLineCancel } from "react-icons/md";
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { link } from '../data/dummy';
+
 
 const Sidebar = () => {
+    const activeMenu = true;
+
     return(
-        <div>sidebar</div>
+        <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+            {activeMenu && ( <>
+                <div className="flex justify-between items-center">
+                    <Link to="/" onClick={() => {}} className="items-center">
+                        <SiShopware /> <span>Shoppy</span>
+                    </Link>
+                </div>
+            </>)}
+        </div>
     )
 }
 
