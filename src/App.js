af8ad8bf372cda26cc792ba536b959843dfd4968
shafiuljony <shafiuljony12@gmail.com>
2022-06-18 20:38:30 +0600

    setUp done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react';
 
-import './App.css';
+
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {TooltipComponent} from "@syncfusion/ej2-react-popups";
 import {FiSettings} from "react-icons/fi";
+
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Pie, Kanban, Editor,ColorPicker, ColorMapping, Customers, Bar, Line, Area, Financial } from "./pages";
+
+import './App.css';
+
 
 const App = () => {
     const activeMenu = true;
@@ -19,43 +26,45 @@ const App = () => {
                 </TooltipComponent>
             </div>
               {activeMenu ? (
-                  <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div>
+                  <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+                      <Sidebar />
+                  </div>
               ): (
-                  <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+                  <div className="w-0 dark:bg-secondary-dark-bg"><Sidebar /></div>
               )}
               <div className={
                   `'dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72'
                       : 'flex-2'}`
               }>
                   <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-                    Navbar
+                    <Navbar />
                   </div>
               </div>
               <div>
                   <Routes>
                       {/* Dashboard*/}
-                      <Route path="/" element="ECommerce" />
-                      <Route path="/ecommerce" element="ECommerce" />
+                      <Route path="/" element={ <Ecommerce />}/>
+                      <Route path="/ecommerce" element={ <Ecommerce />}/>
                       {/* Pages */}
-                      <Route path="/orders" element="Orders" />
-                      <Route path="/employees" element="Employees" />
-                      <Route path="/customers" element="Customers" />
+                      <Route path="/orders" element={ <Orders />}/>
+                      <Route path="/employees" element={ <Employees />}/>
+                      <Route path="/customers" element={ <Customers />} />
 
                       {/* Apps */}
-                      <Route path="/kanban" element="Kanban" />
-                      <Route path="/editor" element="Editor" />
-                      <Route path="/calender" element="Calender" />
-                      <Route path="/color-picker" element="ColorPicker" />
+                      <Route path="/kanban" element={ <Kanban />}/>
+                      <Route path="/editor" element={ <Editor />}/>
+                      <Route path="/calender" element={ <Calendar />}/>
+                      <Route path="/color-picker" element={ <ColorPicker />}/>
 
                       {/* Charts */}
-                      <Route path="/line" element="Line" />
-                      <Route path="/area" element="Area" />
-                      <Route path="/bar" element="Bar" />
-                      <Route path="/pie" element="Pie" />
-                      <Route path="/financial" element="Financial" />
-                      <Route path="/color-mapping" element="ColorMapping" />
-                      <Route path="/pyramid" element="Pyramid" />
-                      <Route path="/stacked" element="Stacked" />
+                      <Route path="/line" element={ <Line />}/>
+                      <Route path="/area" element={ <Area />}/>
+                      <Route path="/bar" element={ <Bar />}/>
+                      <Route path="/pie" element={ <Pie />}/>
+                      <Route path="/financial" element={ <Financial />}/>
+                      <Route path="/color-mapping" element={ <ColorMapping />} />
+                      <Route path="/pyramid" element={<Pyramid />}/>
+                      <Route path="/stacked" element={<Stacked />}/>
                   </Routes>
               </div>
           </div>
---
+++ src/pages/Employees.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Employees = () => {
+    return(
+        <div>Em</div>
+    )
+}
+
+export default Employees;
