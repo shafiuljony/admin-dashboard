310b390598360918fb91c2f754d475b181938b38
shafiuljony <shafiuljony12@gmail.com>
2022-06-19 12:53:07 +0600

    context Api

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import {FiSettings} from "react-icons/fi";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Pie, Kanban, Editor,ColorPicker, ColorMapping, Customers, Bar, Line, Area, Financial } from "./pages";
+import { useStateContext } from "./contexts/ContextProvider";
 
 import './App.css';
 
 
 const App = () => {
-    const activeMenu = true;
-  return(
+    const {activeMenu} = useStateContext();
+
+    return(
       <div>
         <BrowserRouter>
           <div className="flex relative dark:bg-main-dark-bg">
