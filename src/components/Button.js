7801fcd691d8f6512381eb4e0ffb1895b676d045
shafiuljony <shafiuljony12@gmail.com>
2022-06-22 00:25:20 +0600

    problem changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const Button = ({ bgColor, color, size, text, borderRadius}) => {
-  return(
-      <Button
-      type="button"
-      style={{ backgroundColor: bgColor, color,
-        borderRadius }}
-      className={`text-${size} p-3 
-      hover:drop-shadow-xl`}
-      >
-          {text}
-      </Button>
-  )
-}
+import { useStateContext } from '../contexts/ContextProvider';
+
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
+    const { setIsClicked, initialState } = useStateContext();
+
+    return (
+        <button
+            type="button"
+            onClick={() => setIsClicked(initialState)}
+            style={{ backgroundColor: bgColor, color, borderRadius }}
+            className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
+        >
+            {icon} {text}
+        </button>
+    );
+};
 
 export default Button;
