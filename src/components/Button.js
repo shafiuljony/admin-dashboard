f0276f1b2c465da0ae1127b193bf2b723b4fd5c3
shafiuljony <shafiuljony12@gmail.com>
2022-06-21 20:29:19 +0600

    Home page

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const Button = () => {
+const Button = ({ bgColor, color, size, text, borderRadius}) => {
   return(
-      <div>Button</div>
+      <Button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}>Button</Button>
   )
 }
 
