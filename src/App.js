a00ddd92f55ee2750f168dc0a21b1f71498d43f8
shafiuljony <shafiuljony12@gmail.com>
2022-06-27 20:13:10 +0600

    theme stttings

--- src/App.js
+++ src/App.js
@@ -42,6 +42,9 @@ const App = () => {
                     <Navbar />
                   </div>
                   <div>
+                      <ThemeSettings />
+
+
                   <Routes>
                       {/* Dashboard*/}
 
